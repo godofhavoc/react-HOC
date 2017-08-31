9ebc7dcea1b6a4d77bf393f7503640d3205c7c05
tratoli <akshay163@gmail.com>
2017-08-31 10:47:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const monitor = (component) =>
+    class extends Component {
+        componentWillMount() {
+            const startTime = Date.now();
+            this.setState({ startTime });
+            console.log('Mounting ', startTime);
+        }
+
+        componentDidMount() {
+            const endTime = Date.now();
+            console.log('Mounted ', endTime);
+            console.log('Total Time ', endTime - this.state.startTime);
+        }
+
+        render(){
+            return <component {...this.props} />;
+        }
+    }
+
+class App extends Component {
+    render(){
+        return <button>Slick Button</button>;
+    }
+}
+
+App = monitor(App)
+
+export default App
